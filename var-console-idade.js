9e7e9faea1918131b773b4570b372304469efa20
m4ripaiva <160244272+m4ripaiva@users.noreply.github.com>
2024-11-25 11:41:17 -0300

    carga_js-console-idade

---
+++ var-console-idade.js
@@ -0,0 +1,12 @@
+const idadeMíńima = 18;
+const idadeCliente = 16;
+
+if(idadeCliente => idadeMíńima) {
+    console.log("cerveja")
+} else {
+    console.log("suco")
+}
+
+          //condição         //true
+//false          
+console.log(idadeCliente => idadeMíńima ? "cerveja" : "suco")
